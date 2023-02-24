393d144788d7b2f4c5f326c07f51d8258d2f0c5e
Gaetan Bonnemberger <Bonnemberger.gaetan@gmail.com>
2023-02-24 22:29:34 +0100

    Deplacement-axe-X

---
+++ main.py
@@ -0,0 +1,50 @@
+import pygame
+from game import Game
+pygame.init()
+
+#generer la fenetre du jeu
+pygame.display.set_caption("Comet fall game")
+screen = pygame.display.set_mode((1080, 720))
+
+#importer de charger l'arriere plan du jeu
+background = pygame.image.load('assets/bg.jpg')
+
+
+#Charger le jeu
+game = Game()
+
+running = True
+
+#boucle tant que cette condition est vrai
+while running:
+
+    #appliquer l'arriere plan de notre jeu
+    screen.blit(background, (0, -200))
+
+    #appliquer l'image de mon joueur
+    screen.blit(game.player.image, game.player.rect)
+
+    #verifier si le joueur souhaiet aller à gauche ou à droite
+    if game.pressed.get(pygame.K_RIGHT) and game.player.rect.x + game.player.rect.width < screen.get_width():
+        game.player.move_right()
+    elif game.pressed.get(pygame.K_LEFT) and game.player.rect.x > 0:
+        game.player.move_left()
+
+    print(game.player.rect.x)
+
+    #mettre à jour l'écran
+    pygame.display.flip()
+
+    # si le joueur ferme cette fenetre
+    for event in pygame.event.get():
+        #que l'evenement est fermeture de fenetre
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+            print("Fermeture du jeu")
+
+        #detecter si un joueur lache une touche du clavier
+        elif event.type == pygame.KEYDOWN:
+            game.pressed[event.key] = True
+        elif event.type == pygame.KEYUP:
+            game.pressed[event.key] = False
